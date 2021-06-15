19f1edacaff72e7348978aad25b9fe085cc2652e
patdem <patrycjadembinska60@gmail.com>
2021-06-15 10:04:01 +0200

    bug fix - remove dupliation

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -14,14 +14,15 @@ import play.api.test.Helpers._
 class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "HomeController GET" should {
-
+    val welcome = "Welcome to Play"
+    val text = "text/html"
     "render the index page from a new instance of controller" in {
       val controller = new HomeController(stubControllerComponents())
       val home = controller.index().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentType(home) mustBe Some(text)
+      contentAsString(home) must include (welcome)
     }
 
     "render the index page from the application" in {
@@ -29,8 +30,8 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       val home = controller.index().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentType(home) mustBe Some(text)
+      contentAsString(home) must include (welcome)
     }
 
     "render the index page from the router" in {
@@ -38,8 +39,8 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       val home = route(app, request).get
 
       status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentType(home) mustBe Some(text)
+      contentAsString(home) must include (welcome)
     }
   }
 }
